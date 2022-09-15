2e438e0f5ab55aee013ac773555bd8edc3b23959
mitchelldirt <mitchellmudd@outlook.com>
2022-09-15 07:34:07 -0400

    Setting the main branch to display as under construction until I'm at a point where I have something to show

--- City Ranker/src/App.jsx
+++ City Ranker/src/App.jsx
@@ -15,21 +15,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>
-          GET SAVED
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>This app is under construction</h1> 
+     <p>Please visit <a target='_blank' href='https://github.com/mitchelldirt/City-Ranker'>the GitHub respository</a> for more info :)</p>
     </div>
   )
 }
