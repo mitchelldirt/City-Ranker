7e4ce53e9192ed9fd3d6b3f03d58066c82004218
Mitchell Mudd <mitchellmudd@outlook.com>
2022-09-17 16:34:43 -0400

    Got my API to request and return data from Google

--- api/routes/google.js
+++ api/routes/google.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/', function(req, res, next) {
-  res.send('Google API returns content');
+router.get('/:address', function (req, res, next) {
+
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.address}&key=AIzaSyDkJCkdqhEg7a21pzd39GZ3UaqFFyp59C4`;
+
+    let options = { method: 'GET' };
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => res.send(json))
+        .catch(err => console.error('error:' + err));
 });
 
 module.exports = router;
