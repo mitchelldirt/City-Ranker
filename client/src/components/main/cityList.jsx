5195f84028655b7b97998d64bb65c652be3c21e7
mitchelldirt <mitchellmudd@outlook.com>
2022-09-22 11:04:44 -0400

    All buttons are working now :) MVP is basically done

--- client/src/components/main/cityList.jsx
+++ client/src/components/main/cityList.jsx
@@ -4,7 +4,15 @@ import getImage from '../../services/getImage';
 import getWalkscore from '../../services/walkscore'
 import CityRow from './cityRow/cityRow';
 
-let nextId = 0;
+let nextId = 3;
+
+const testCityList = [
+    { imageURL: 'https://images.pexels.com/photos/432361/pexels-photo-432361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', cityName: 'Portland, OR USA', walkScore: '67', bikeScore: '49', transitScore: '83', id: 0 },
+    { imageURL: 'https://images.pexels.com/photos/1796730/pexels-photo-1796730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', cityName: 'Seattle, WA USA', walkScore: '74', bikeScore: '60', transitScore: '71', id: 1 },
+    { imageURL: 'https://images.pexels.com/photos/3876958/pexels-photo-3876958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', cityName: 'Tucson, AZ USA', walkScore: '43', bikeScore: '35', transitScore: '66', id: 2 }
+]
+
+
 
 // Use the user input (should be a city name) to call 3 APIs and create the city object
 async function createCityObject(userInput) {
@@ -27,7 +35,7 @@ async function createCityObject(userInput) {
 
 export default function CityList() {
     const [name, setName] = useState('');
-    const [cities, setCities] = useState([])
+    const [cities, setCities] = useState(testCityList)
 
     function handleDelete(badCity) {
 
@@ -75,19 +83,30 @@ export default function CityList() {
                             walkScore={city.walkScore}
                             bikeScore={city.bikeScore}
                             transitScore={city.transitScore} />
-                            <button onClick={() => { setCities(cities.filter(c => c.id !== city.id)) }}>Delete</button>
-                            <button onClick={() => {
-                                let insertAt = cities.indexOf(city) - 1;
-                                if (insertAt => 0) {
-                                    const nextCity = [
-                                        ...cities.slice(0, insertAt),
-                                        city,
-                                        ...cities.slice(insertAt)
-                                    ]
-                                    setCities(nextCity);
-                                }
-                                return;
-                            }}>Up Shift</button>
+
+                        <button onClick={() => { setCities(cities.filter(c => c.id !== city.id)) }}>Delete</button>
+
+                        <button onClick={() => {
+                            let insertAt = cities.indexOf(city) - 1;
+                            let nextCities = [...cities]
+                            if (insertAt => 0) {
+                                nextCities.splice(cities.indexOf(city), 1);
+                                nextCities.splice(insertAt, 0, city);
+                                setCities(nextCities);
+                            }
+                            return;
+                        }}>Up Shift</button>
+                        <button onClick={() => {
+                            let insertAt = cities.indexOf(city) + 1;
+                            let nextCities = [...cities]
+                            if (insertAt < cities.length) {
+                                nextCities.splice(cities.indexOf(city), 1);
+                                nextCities.splice(insertAt, 0, city);
+                                setCities(nextCities);
+                            }
+                            return;
+                        }}>Down Shift</button>
+
                     </li>
                 ))}
             </div>
