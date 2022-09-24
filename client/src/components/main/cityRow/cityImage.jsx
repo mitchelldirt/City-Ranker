a86b101de863dc2372c13968d745bfa0df4c2236
Mitchell Mudd <mitchellmudd@outlook.com>
2022-09-23 21:43:33 -0400

    Changed the score percent bar - updated UI

--- client/src/components/main/cityRow/cityImage.jsx
+++ client/src/components/main/cityRow/cityImage.jsx
@@ -1,19 +1,11 @@
-export default function CityInformation({imgURL, cityName}) {
-    const divStyles = {
-        height: '100px',
-        width: '100px'
-    }
-
+export default function CityImg({imgURL}) {
     const imageStyles = {
         width: '200px',
         height: '200px'
     }
     return (
         <>
-
             <img style={imageStyles} src={imgURL} />
-            <p>{cityName}</p>
-
         </>
     )
 }
--- client/src/components/main/cityRow/cityRow.jsx
+++ client/src/components/main/cityRow/cityRow.jsx
@@ -1,20 +1,31 @@
-import CityInformation from './cityInformation'
+import CityImg from './cityImage'
 import Score from './scores'
+import Pie from './scores2'
 
 export default function CityRow({ imgURL, cityName, walkScore, bikeScore, transitScore }) {
     const cityCard = {
         display: 'flex',
-        border: '2px solid gray',
         height: 'fit-content'
     }
-    
+
     return (
         <>
             <div style={cityCard}>
-                <CityInformation imgURL={imgURL} cityName={cityName} />
-                <Score degrees={(180 / 100) * walkScore} percent={walkScore} />
+                <CityImg imgURL={imgURL} />
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                    <p>{cityName}</p>
+                    <div>
+                        <Pie percentage={walkScore} colour="blue" />
+                        <Pie percentage={bikeScore} colour="blue" />
+                        <Pie percentage={transitScore} colour="blue" />
+                    </div>
+                </div>
+                {/*<Score degrees={(180 / 100) * walkScore} percent={walkScore} />
                 <Score degrees={(180 / 100) * bikeScore} percent={bikeScore} />
-                <Score degrees={(180 / 100) * transitScore} percent={transitScore} />
+                <Score degrees={(180 / 100) * transitScore} percent={transitScore} >*/}
             </div>
         </>
     )
