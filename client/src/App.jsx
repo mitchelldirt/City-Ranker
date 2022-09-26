0e76e8679b56bcf64d26a22af32ad52d0d7c1e7a
mitchelldirt <mitchellmudd@outlook.com>
2022-09-26 08:05:18 -0400

    Started implementing auth :)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
-import getGeoInformation from './services/geocode'
-import getImage from './services/getImage'
-import getWalkscore from './services/walkscore'
+import { useState, useEffect } from 'react'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
-import CityRow from './components/main/cityRow/cityRow'
 import CityList from './components/main/cityList'
+import { supabase } from './services/supabaseClient'
+import Auth from './components/header/auth'
+import Account from './components/header/account'
 
 import './App.css'
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
 
   return (
     <div className="App">
       <Header />
+      <Auth />
       <div className='wrapper'>
-      <CityList key="cityList"/>
-  </div>
+        <CityList key="cityList" />
+      </div>
       <Footer />
     </div>
   )
