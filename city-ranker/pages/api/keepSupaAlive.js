8cd265f4ed89f857aacec8c5f4a46adf8359f34a
mitchelldirt <mitchellmudd@outlook.com>
2023-04-11 09:29:48 -0400

    Trying the junk table instead

--- city-ranker/pages/api/keepSupaAlive.js
+++ city-ranker/pages/api/keepSupaAlive.js
@@ -2,8 +2,8 @@ import { supabase } from '../../services/supabaseClient';
 
 export default async function handler(request, response) {
   try {
-    const supaResponse = await supabase.from('profiles').insert([
-      { username: 'test' }
+    const supaResponse = await supabase.from('junk').insert([
+      { id: new Date().getTime() }
     ])
 
     response.status(200).json({ body: supaResponse })
