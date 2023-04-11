dffb9e17718bca40b5a5ddab60ae27ee75fd8095
mitchelldirt <mitchellmudd@outlook.com>
2023-04-11 09:01:56 -0400

    trying to see supa response

--- city-ranker/pages/api/keepSupaAlive.js
+++ city-ranker/pages/api/keepSupaAlive.js
@@ -2,12 +2,13 @@ import { supabase } from '../../services/supabaseClient';
 
 export default async function handler(request, response) {
   try {
-    await supabase.from('profiles').insert([
+    const supaResponse = await supabase.from('profiles').insert([
       { id: 'test1234', username: 'test' }
     ])
-
+    
+    response.status(200).json({ body: supaResponse })
   } catch (error) {
     response.status(500).json({ error: error.message })
   }
-  response.status(200).json({ body: 'ok' })
+  
 }
