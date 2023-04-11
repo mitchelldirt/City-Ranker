3fe45243971b8f13b6372161a046c6d0b7d1d015
mitchelldirt <mitchellmudd@outlook.com>
2023-04-11 09:06:28 -0400

    Seeing if the UUID will get set by default

--- city-ranker/pages/api/keepSupaAlive.js
+++ city-ranker/pages/api/keepSupaAlive.js
@@ -3,9 +3,9 @@ import { supabase } from '../../services/supabaseClient';
 export default async function handler(request, response) {
   try {
     const supaResponse = await supabase.from('profiles').insert([
-      { id: 'test1234', username: 'test' }
+      { username: 'test' }
     ])
-    
+
     response.status(200).json({ body: supaResponse })
   } catch (error) {
     response.status(500).json({ error: error.message })
